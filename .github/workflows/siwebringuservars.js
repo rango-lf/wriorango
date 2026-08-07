a7cb68cc78dc88ec92bf244f917da6995f40b6b8
Rango ! <baloney7987@gmail.com>
2026-08-07 17:40:16 -0400

    Add files via upload

---
+++ .github/workflows/siwebringuservars.js
@@ -0,0 +1,5 @@
+var name = "Name";
+var thing = "besties";
+var character = "This character";
+var media = "This media";
+var image = "https://dears.nekoweb.org/siwebring/siblank.png"
---
+++ .github/workflows/yume.js
@@ -0,0 +1,56 @@
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("YumeRing").innerHTML = `
+
+
+<div class="yumering">
+<div id="img"></div>
+
+<div id="inbox">
+this webmaster is a <b>sharing</b> <b>Gojo Satoru</b> <b>self-shipper</b><br>
+<a href="https://moekkis.neocities.org/yumeclique.html">join the yumeclique !</a>
+</div>
+</div>
+
+
+<style></style>
+
+<style>
+
+.yumering {
+    display:flex;
+    box-sizing:border-box;
+    gap: 7.5px; /* Scaled down from 10px */
+    justify-content:center;
+    align-items:center;
+    padding: 7.5px; /* Scaled down from 10px */
+    height: 82.5px; width: 225px; /* Scaled down from 110px and 300px */
+    border-radius: 3.75px; /* Scaled down from 5px */
+    border: 1px solid black;
+    font-smooth:never;
+    -webkit-font-smoothing:none;
+    background: linear-gradient(to bottom, transparent, white), url('https://files.catbox.moe/dbv9b2.gif');
+    background-size: 75px; /* Scaled down from 100px */
+}
+
+.yumering #img {
+    width: 67.5px; height: 67.5px; /* Scaled down from 90px */
+    background-image: url('https://i.pinimg.com/736x/74/2f/8d/742f8db3f4349fa0fea330ce1f70fbdb.jpg');
+    border:1px solid black;
+    border-radius: 1.5px; /* Scaled down from 2px */
+    background-size:cover;
+    background-position:center;
+}
+
+.yumering #inbox {
+    width: 142.5px; /* Scaled down from 190px */
+    font-size: 9px; /* Scaled down from 12px */
+    color: black;
+}
+
+p {
+  color: black;
+}
+ 
+</style>
+
+`;});
